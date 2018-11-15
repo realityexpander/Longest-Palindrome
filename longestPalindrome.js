9e30dba4c0aca22f619da8a0d51ca608825c823e
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-15 09:21:08 -0600

    Create longestPalindrome.js

---
+++ longestPalindrome.js
@@ -0,0 +1,12 @@
+function longestPalindrome (string) {
+  // Write your code here, and
+  // return your final answer.
+}
+
+// for each char
+//   check next char, if it matches current char, keep checking
+//     once errored, then check max length & string
+//   check surrounding 2 chars, if match, keep checking
+//     once errored, then check max length & string
+//
+// check char is within bounds

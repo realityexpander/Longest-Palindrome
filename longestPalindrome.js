672d729299afba386dbfba9f42158f9448a7a9e7
Chris Athanas <realityexpanderdev@gmail.com>
2018-11-15 21:45:01 -0600

    Update longestPalindrome.js

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -1,11 +1,13 @@
 // for each char
 //   check next char, if it matches current char, keep checking
-//     once errored, then check max length & string
+//     once not found match, then check max length & string
 //   check surrounding 2 chars, if match, keep checking
-//     once errored, then check max length & string
+//     once not found match, then check max length & string
 //
 // helper fn: checkChar
 //   check char is within bounds
+// Find Palidrome
+//   starts at a central char idx, and then grows outwardly checking each side
 
 function longestPalindrome (string) {
 
@@ -24,6 +26,8 @@ function longestPalindrome (string) {
       foundString = findPalindrome(i-1, i+1, string, foundString);
     }
   }
+
+  return foundString;
   
   // return char within bounds
   function checkChar(idx, string) {
@@ -34,20 +38,24 @@ function longestPalindrome (string) {
 
   function findPalindrome(left, right, string, curFoundString) {
     let foundMatch = true;
-    let searchString = '';
+    let foundString = '';
     while(foundMatch) {
       if( checkChar(left, string) === checkChar(right, string)) {
+        foundString = string.substring(left,right+1);
         left--;
         right++;
       }
       else
         foundMatch = false;
     }
-    foundString = string.substring(left,right+1);
-    if (foundString.length > maxLength) {
+    
+    if (foundString.length > curFoundString.length) {
       return foundString;
+    } else {
+      return curFoundString;
     }
   }
 
 }
 
+console.log(longestPalindrome("My dad is a racecar athlete"));
